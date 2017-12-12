2a8d8a69bdef3f8463cd3d3a93b3149df4aba8f8
Aubree Abril <aubree.abril@gmail.com>
2017-12-12 00:37:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ if (name === "Susan") {
   console.log("Expected the name to be Susan");
 };
 
-if (height === "74") {
+if (height === "70") {
   console.log("The height is correct");
-} else { 
-  console.log("Expected the height to be 70");
+} else {
+  console.log("Expected the height to be 74");
 }
 
 // Don't worry about this
